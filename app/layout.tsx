6a722bd31b17e3987e71caa59401e444da065413
ybad6263-jpg <ybad6263@gmail.com>
2026-03-08 22:25:19 +0630

    anal commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
+
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
