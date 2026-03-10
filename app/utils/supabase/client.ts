1693235f866585fa147c41e1bc7015305ba16e17
ybad6263-jpg <ybad6263@gmail.com>
2026-03-10 17:20:24 +0630

    supabase client fix

--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -1,9 +1,11 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from '@supabase/supabase-js'
 
-export function createClient()
-{
-    return createBrowserClient(
-        process.env.Next_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    )
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  // This will help you see in Vercel logs if the variables are even being read
+  console.error("Missing Supabase Environment Variables!")
 }
+
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!)
