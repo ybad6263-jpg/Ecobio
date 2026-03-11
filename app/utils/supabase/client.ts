e7c6e704478b4ad7f35f194b9eda3b7ed5cf91d3
ybad6263-jpg <ybad6263@gmail.com>
2026-03-11 15:33:58 +0630

    API error

--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { cookies } from 'next/headers'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://ahtvxahmxkipxymoxdmg.supabase.co'
+const supabaseAnonKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFodHZ4YWhteGtpcHh5bW94ZG1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwODMyNTEsImV4cCI6MjA4ODY1OTI1MX0.CBnEUzlTYsMhl9YJb1N1-NBp6cLX9GiBRe7VyGUlQKg'
 
 if (!supabaseUrl || !supabaseAnonKey) {
   // This will help you see in Vercel logs if the variables are even being read
