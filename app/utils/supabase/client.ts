91bcad9df4371b3c68e0f9fd486d5fbe26612cea
ybad6263-jpg <ybad6263@gmail.com>
2026-03-11 15:40:28 +0630

    service role key

--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 import { cookies } from 'next/headers'
 
 const supabaseUrl = 'https://ahtvxahmxkipxymoxdmg.supabase.co'
-const supabaseAnonKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFodHZ4YWhteGtpcHh5bW94ZG1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwODMyNTEsImV4cCI6MjA4ODY1OTI1MX0.CBnEUzlTYsMhl9YJb1N1-NBp6cLX9GiBRe7VyGUlQKg'
+const supabaseAnonKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFodHZ4YWhteGtpcHh5bW94ZG1nIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MzA4MzI1MSwiZXhwIjoyMDg4NjU5MjUxfQ.uwMBnlIlpCRW5SqwdKBjKvkJtEwZwDJkknmXr7F1Mu8'
 
 if (!supabaseUrl || !supabaseAnonKey) {
   // This will help you see in Vercel logs if the variables are even being read
