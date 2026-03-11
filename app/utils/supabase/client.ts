c99405d3dc80e20d8b69f38486f67ef029edc7bb
ybad6263-jpg <ybad6263@gmail.com>
2026-03-11 15:30:17 +0630

    databease route.ts added ex yes again API Key hello

--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { cookies } from 'next/headers'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 if (!supabaseUrl || !supabaseAnonKey) {
   // This will help you see in Vercel logs if the variables are even being read
