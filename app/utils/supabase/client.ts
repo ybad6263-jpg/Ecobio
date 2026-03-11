f32c9f56b6d993ab079261c4e57903bcb211c5fa
ybad6263-jpg <ybad6263@gmail.com>
2026-03-11 16:05:58 +0630

    key normal

--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { cookies } from 'next/headers'
 
-const supabaseUrl = 'https://ahtvxahmxkipxymoxdmg.supabase.co'
-const supabaseAnonKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFodHZ4YWhteGtpcHh5bW94ZG1nIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MzA4MzI1MSwiZXhwIjoyMDg4NjU5MjUxfQ.uwMBnlIlpCRW5SqwdKBjKvkJtEwZwDJkknmXr7F1Mu8'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
   // This will help you see in Vercel logs if the variables are even being read
